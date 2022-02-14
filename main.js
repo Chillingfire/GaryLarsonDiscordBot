339e0e6281b1a2dc1b70e899bc641bc4605499d0
Daniel Kantor <daniel.kantor2009@gmail.com>
2022-02-13 19:59:21 -0800

    Added more quotes

--- main.js
+++ main.js
@@ -12,7 +12,18 @@ const quotes = ["*\"Well, I'm addicted... Have you tried Carol's sheep dip?\"*",
 "*\"Wait a minute! Isn't anyone here a real sheep?\"*",
 "*\"And now Edgar's gone... Something's going on around here\"*",
 "*\"C'mon, c'mon - it's either one or the other\"*",
-"*\"Well, we might as well put it on board - although I'm not sure what use we'll have for a box of rusty nails, broken glass, and throwing darts.\"*"]
+"*\"Well, we might as well put it on board - although I'm not sure what use we'll have for a box of rusty nails, broken glass, and throwing darts.\"*",
+"*\"Open the gate! It\'s a big weiner dog!\"*",
+"*\"Gee whiz... you mean I get a THIRD wish, too?\"*",
+"*\"While their owners sleep, nervous little dogs prepare for their day\"*",
+"*\"Oh, great! Now there goes my hat!",
+"*\"Well, there is some irony in all this, you know... I mean, we both lose a contact at the same time?!\"*",
+"*\"Say... wasn\'t there supposed to be a couple of holes punched in this thing?\"*",
+"*\"Nothing yet... How about you, Newton?\"*",
+"*\"I\'ve got an idea... How many here have ever seen Alfred Hitchcock's The Birds?\"*",
+"*\"And now... Can dogs really talk?... We found one who\'s willing to try, right after this message\"*",
+"*\"Where the buffalo cruise\"*"]
+
 
 client.on('message', message =>{
     if (!message.content.startsWith(prefix) || message.author.bot) return;
