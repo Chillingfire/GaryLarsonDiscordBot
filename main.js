40423c7befa95a07f9d7d1a363d6dc0741394971
Daniel Kantor <daniel.kantor2009@gmail.com>
2022-02-19 10:24:45 -0800

    Token invalid error. Trying to push to see if it fixes it

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ client.once('ready', () => {
     console.log('Gary Larson Daily is online!');
 });
 
-const quotes = ["*\"Well, I'm addicted... Have you tried Carol's sheep dip?\"*",
+/*
+["*\"Well, I'm addicted... Have you tried Carol's sheep dip?\"*",
 "*\"Wait a minute! Isn't anyone here a real sheep?\"*",
 "*\"And now Edgar's gone... Something's going on around here\"*",
 "*\"C'mon, c'mon - it's either one or the other\"*",
@@ -16,14 +17,22 @@ const quotes = ["*\"Well, I'm addicted... Have you tried Carol's sheep dip?\"*",
 "*\"Open the gate! It\'s a big weiner dog!\"*",
 "*\"Gee whiz... you mean I get a THIRD wish, too?\"*",
 "*\"While their owners sleep, nervous little dogs prepare for their day\"*",
-"*\"Oh, great! Now there goes my hat!",
+"*\"Oh, great! Now there goes my hat!\"*",
 "*\"Well, there is some irony in all this, you know... I mean, we both lose a contact at the same time?!\"*",
 "*\"Say... wasn\'t there supposed to be a couple of holes punched in this thing?\"*",
 "*\"Nothing yet... How about you, Newton?\"*",
 "*\"I\'ve got an idea... How many here have ever seen Alfred Hitchcock's The Birds?\"*",
 "*\"And now... Can dogs really talk?... We found one who\'s willing to try, right after this message\"*",
 "*\"Where the buffalo cruise\"*"]
+*/
 
+var quotes;
+
+const fs = require('fs');
+fs.readFile('farsidequotes.txt', function(err, data) {
+    if(err) throw err;
+    quotes = data.toString().split("\n");
+})
 
 client.on('message', message =>{
     if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -33,6 +42,10 @@ client.on('message', message =>{
 
     if (command === 'ping') {
         message.channel.send(quotes[Math.floor(Math.random() * quotes.length)]);
+    }
+
+    if (command === 'cow tools') {
+        message.channel.send("Cow tools");
     }
 })
 
